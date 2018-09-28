4f7894ce1f43a167cbc02d6d8973d0c5276f766f
Andrew Peterson <peter343@purdue.edu>
2018-09-28 17:48:13 -0400

    Added some helpers and trying to organize files

---
+++ Mirage Companion App/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Mirage Companion App
+//
+//  Created by Andrew Peterson on 9/28/18.
+//  Copyright © 2018 Mirage. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+let wifiStatusService = CBUUID.init(string: "c30b8a67-d1a2-40e3-a407-9f126001b6cc")
+let wifiStatChrcUUID = CBUUID.init(string: "43ec4c0c-6d53-44a6-ae81-a6e6518269d4")
+let wifiSSIDChrcUUID = CBUUID.init(string: "1249e241-7624-4158-acd5-86226f7637c8")
+let wifiPASSChrcUUID = CBUUID.init(string: "290d03b7-6e73-4e1e-ab70-ac8997cc0506")
