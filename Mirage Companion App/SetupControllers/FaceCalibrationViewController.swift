d46d44ebe22e1292a2361a0771b2722de01cb665
Andrew Peterson <peter343@purdue.edu>
2018-11-29 14:26:16 -0500

    Adding to commit

--- Mirage Companion App/SetupControllers/FaceCalibrationViewController.swift
+++ Mirage Companion App/SetupControllers/FaceCalibrationViewController.swift
@@ -11,51 +11,28 @@ import UIKit
 class FaceCalibrationViewController: UIViewController {
     
     var user: User!
-    var calibrationFile: String!
-
+//    var calibrationFile: String!
+    var num: Int = 0
+    var editingProfile: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
     }
     
     @IBAction func startPressed(_ sender: Any) {
         
         // TODO: Send bluetooth signal to Pi to start facial calibration
-        DispatchQueue.main.async {
-            self.startCalibration() { completed in
-                if (completed) {
-                    //self.doSegue()
-                    //self.performSegue(withIdentifier: "CalibrationToAddress", sender: nil)
-                }
-            }
-        }
-        doSegue()
-    }
-    
-    func doSegue() {
-      performSegue(withIdentifier: "CalibrationToAddress", sender: nil)
+//        SystemInfo.shared().startCalibration() { completed in
+//            if (completed) {
+//                DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "CalibrationToAddress", sender: nil)
+//                }
+//            }
+//        }
     }
     
     @IBAction func cancelPressed(_ sender: Any) {
         self.navigationController?.popToRootViewController(animated: true)
-    }
-    
-    func startCalibration(completion: @escaping (Bool) -> Void) {
-        let url = URL(string: "http://" + ipAddr + ":5000/setup/newuser/" + calibrationFile)
-            let task = URLSession.shared.dataTask(with: url!) { data, response, error in
-                guard let data = data, error == nil else {
-                    print(error?.localizedDescription ?? "No error description")
-                    return
-                }
-                let response = String(data: data, encoding: .utf8)
-                print(response ?? "No valid response")
-                if (response == "Done") {
-                    completion (true)
-                } else {
-                    completion (false)
-                }
-            }
-            task.resume()
     }
     
     // MARK: - Navigation
@@ -66,7 +43,8 @@ class FaceCalibrationViewController: UIViewController {
         // Pass the selected object to the new view controller.
         let dest = segue.destination as! AddressSetupViewController
         dest.user = self.user
-        dest.userFile = self.calibrationFile
+        dest.editingProfile = self.editingProfile
+//        dest.userFile = self.calibrationFile
     }
     
 
